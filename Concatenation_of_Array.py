c88e3154d89ec17d71ef795a43a41ffa04dd704e
brian <briansegs@gmail.com>
2023-03-20 09:13:07 -0400

    refactor: Concatenation of array tests

--- Concatenation_of_Array.py
+++ Concatenation_of_Array.py
@@ -7,12 +7,13 @@ class Solution(object):
         return nums + nums
 
 
-solution1 = Solution()
-solution2 = Solution()
-
 def test_solutions():
-    assert solution1.getConcatenation([1,2,1]) == [1, 2, 1, 1, 2, 1], "Should be [1, 2, 1, 1, 2, 1]"
-    assert solution2.getConcatenation([1,3,2,1]) == [1, 3, 2, 1, 1, 3, 2, 1], "Should be [1, 3, 2, 1, 1, 3, 2, 1]"
+    solution = Solution()
+
+    assert solution.getConcatenation([]) == [], "Should be []"
+    assert solution.getConcatenation([1,2,1]) == [1, 2, 1, 1, 2, 1], "Should be [1, 2, 1, 1, 2, 1]"
+    assert solution.getConcatenation([1,3,2,1]) == [1, 3, 2, 1, 1, 3, 2, 1], "Should be [1, 3, 2, 1, 1, 3, 2, 1]"
+
 
 if __name__ == "__main__":
     test_solutions()
